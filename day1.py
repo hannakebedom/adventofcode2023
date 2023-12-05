f97273e92367d8776d6423eda6332b508517955e
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-12-04 22:27:44 -0500

    day 1 - part 2

--- day1.py
+++ day1.py
@@ -1,8 +1,9 @@
 # Day 1: Trebuchet?! (December 4th, 2023)
 
-file = open('input/day1.txt', 'r')
-lines = file.readlines()
+from utils import read_input
+lines = read_input("day1.txt")
 
+# Part 1
 def sum_calibration_values():
     sum = 0
     for line in lines:
@@ -24,5 +25,55 @@ def sum_calibration_values():
         sum += int(num)
     return sum
 
-print("result: ", sum_calibration_values())
-            
+# print("result: ", sum_calibration_values())
+
+# Part 2
+DIGITS = {"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7', "eight": '8', "nine": '9'}
+def sum_calibration_letter_digits():
+    sum = 0
+    
+    for line in lines:
+        first = ''
+        last = ''
+        i = 0
+
+        while i < len(line) - 1:
+            char = line[i]
+
+            if char.isnumeric() and not first:
+                first = char
+                i += 1
+            elif char.isnumeric() and first:
+                last = char
+                i += 1
+            elif char.isalpha():
+                num = 0
+                if DIGITS.get(line[i:i+3]):
+                    num = DIGITS.get(line[i:i+3])
+                    i += 2
+                elif DIGITS.get(line[i:i+4]):
+                    num = DIGITS.get(line[i:i+4])
+                    i += 3
+                elif DIGITS.get(line[i:i+5]):
+                    num = DIGITS.get(line[i:i+5])
+                    i += 4
+                else:
+                    i += 1
+                    continue
+                
+                if not first:
+                    first = num
+                else:
+                    last = num
+
+        if first and last:
+            number = first + last
+        else:
+            number = first + first
+        
+        sum += int(number)
+
+    return sum
+
+print("result (part 2): ", sum_calibration_letter_digits())
+
---
+++ utils.py
@@ -0,0 +1,4 @@
+def read_input(file_name):
+    file = open(f'input/{file_name}')
+    lines = file.readlines()
+    return lines
