4966498adeb1e9b8b9b851211cb059e4b5bdf9c0
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-12-06 22:03:02 -0500

    day 6 - part 1

--- day1.py
+++ day1.py
@@ -25,7 +25,7 @@ def sum_calibration_values():
         sum += int(num)
     return sum
 
-# print("result: ", sum_calibration_values())
+print("result: ", sum_calibration_values())
 
 # Part 2
 DIGITS = {"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7', "eight": '8', "nine": '9'}
--- day6.py
+++ day6.py
@@ -1,20 +1,32 @@
 # Day 6: Wait for it (December 6th, 2023)
 
 from utils import read_input
-lines = read_input("day6ex.txt")
+lines = read_input("day6.txt")
 
 def day6():
     times = lines[0].split()[1:]
     distances = lines[1].split()[1:]
 
-    for i in range(1, len(times)):
+    result = 1
+    for i in range(len(times)):
         time = int(times[i])
-        distance = int(distances[i])
+        record_distance = int(distances[i])
+        ways_to_beat_record = 0
+
+        print("race")
+        print("time: ", time)
+        print("record distance: ", record_distance)
+
         for j in range(1, time):
-            print(i)
+            speed = j # in mm per ms
+            remaining_time = time - j # in ms 
+            distance = speed * remaining_time # in mm
+            if distance > record_distance:
+                ways_to_beat_record += 1
+        
+        result *= ways_to_beat_record
+    
+    return result
 
-    print("time: ", time)
-    print("distance: ", distance)
-
-day6()
+print(day6())
 
