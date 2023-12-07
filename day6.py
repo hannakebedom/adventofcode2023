425f9a282fc05fde46c0701ce2488667db74f6f0
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-12-06 22:19:55 -0500

    day 6 - part 2

--- day6.py
+++ day6.py
@@ -3,7 +3,7 @@
 from utils import read_input
 lines = read_input("day6.txt")
 
-def day6():
+def day6_p1():
     times = lines[0].split()[1:]
     distances = lines[1].split()[1:]
 
@@ -28,5 +28,25 @@ def day6():
     
     return result
 
-print(day6())
+print(day6_p1())
+
+def day6_p2():
+    time = int("".join(lines[0].split()[1:]))
+    record_distance = int("".join(lines[1].split()[1:]))
+    ways_to_beat_record = 0
+
+    for j in range(1, time):
+        speed = j # in mm per ms
+        remaining_time = time - j # in ms 
+        distance = speed * remaining_time # in mm
+        if distance > record_distance:
+            ways_to_beat_record += 1
+    
+    return ways_to_beat_record
+
+print(day6_p2())
+
+
+
+
 
